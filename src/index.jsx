de12299b1413117bd3af8932bf37cce4f8e08e40
bruvlee <lee.lichtsinn@gmail.com>
2017-02-06 20:16:13 -0500

    created src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  <div>
+    <h1>WORK</h1>
+    <h3>E.S.P. TV</h3>
+  </div>
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);

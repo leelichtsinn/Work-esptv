4beb33c2d4194d8e1471b365e2b733615056a43d
bruvlee <lee.lichtsinn@gmail.com>
2017-02-06 19:53:27 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = require('app');
+
+// Use public folder as the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Send any route to index.html where the app is mounted
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(3000, () => console.log('App running on localhost:3000'));
